8e23eaf14c376a566fb2f3e188a5577a3692ff30
Jack Yeh <jack.yeh@gmail.com>
2016-01-17 23:46:26 -0700

    add functionality: sending messages from the client to the server; in this case sending back vote results in the form of button clicks

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http        = require('http');
 const express     = require('express');
 const app         = express();
 
+var votes = {};
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res){
@@ -25,8 +27,17 @@ io.on('connection', function(socket) {
 
   socket.emit('statusMessage', 'You have connected.');
 
+  socket.on('message', function(channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+     console.log(votes);
+    }
+  });
+
   socket.on('disconnect', function() {
     console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
     io.sockets.emit('usersConnected', io.engine.clientsCount);
   });
 });
