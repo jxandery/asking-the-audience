77d008ba4df88a8b93ea9802dbb88d4c3e37f8b7
Jack Yeh <jack.yeh@gmail.com>
2016-01-17 23:27:11 -0700

    setup communication between the client and the server

--- server.js
+++ server.js
@@ -18,4 +18,13 @@ var server = http.createServer(app)
 const socketIo    = require('socket.io');
 const io          = socketIo(server);
 
+io.on('connection', function(socket) {
+  console.log('A user has connected', io.engine.clientsCount);
+
+  socket.on('disconnect', function() {
+    console.log('A user has disconnected.', io.engine.clientsCount)
+  });
+});
+
+
 module.exports = server;
